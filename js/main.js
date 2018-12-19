a36119c225143c7f50f1dac105d832388de80ea4
Ernest D. Peterson <ernest@ernestpeterson.com>
2018-12-18 18:30:58 -0600

    Tests and first notes completed.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 /* eslint-env jquery */
 
 $('#theHeader').html('<h1>Udemy AJAX</h1>');
-$('#theFooter').html('Some Stuff In The Footer');
+$('#theFooter').html('<h3>Some Stuff In The Footer</h3>');
