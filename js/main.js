fb6b9d7b947255fd27eb438ca5638ef409d8620b
Ernest D. Peterson <ernest@ernestpeterson.com>
2018-12-19 08:35:22 -0600

    Added an array.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 /* eslint-env jquery */
 
+var myObj = {
+    'people': [
+        {
+            'firstName': 'Ernest',
+            'lastName': 'Peterson',
+            'age': 48
+        },
+        {
+            'firstName': 'John',
+            'lastName': 'Smith',
+            'age': 21
+        }
+    ]
+};
+
+for (var i = 0; i < myObj.people.length; i++) {
+    myfunc(
+        myObj.people[i].firstName + '<br>' + 
+        myObj.people[i].lastName
+    );
+}
+
+function myfunc(somevar) {
+    $('#output').append(somevar + '<br><br>');
+
+}
+
 $('#theHeader').html('<h1>Udemy AJAX</h1>');
 $('#theFooter').html('<h3>Some Stuff In The Footer</h3>');
