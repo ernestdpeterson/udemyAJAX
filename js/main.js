fbb296e877b4e8a2a7007971584671299e013f01
Ernest D. Peterson <Myself@localhost.localdomain>
2018-12-22 11:25:16 -0600

    Finished this course.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,71 @@
 /* eslint-env jquery */
 
-var myObj = {
-    'people': [
-        {
-            'firstName': 'Ernest',
-            'lastName': 'Peterson',
-            'age': 48
-        },
-        {
-            'firstName': 'John',
-            'lastName': 'Smith',
-            'age': 21
-        }
-    ]
-};
+// newJason created from myjson.com using myObj bellow
+// var newJason = "https://api.myjson.com/bins/1dango";
 
-for (var i = 0; i < myObj.people.length; i++) {
-    myfunc(
-        myObj.people[i].firstName + '<br>' + 
-        myObj.people[i].lastName
-    );
+var bigData = "https://randomuser.me/api/?results=5";
+var xhr = new XMLHttpRequest();
+var btn = document.getElementById("loadMore");
+btn.addEventListener("click", function() {
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            parseData(JSON.parse(xhr.response));
+        }
+    };
+    xhr.open("GET", bigData);
+    xhr.send();
+});
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        parseData(JSON.parse(xhr.response));
+    }
+};
+xhr.open("GET", bigData);
+xhr.send();
+
+// var myObj = {
+//     "people": [
+//         {
+//             "firstName": "Ernest",
+//             "lastName": "Peterson",
+//             "age": 48,
+//             "pass": true
+//         },
+//         {
+//             "firstName": "John",
+//             "lastName": "Smith",
+//             "age": 21,
+//             "pass": false
+//         },
+//         {
+//             "firstName": "Benjamin",
+//             "lastName": "Peterson",
+//             "age": 15,
+//             "pass": false
+//         }
+//     ]
+// };
+
+function parseData(data) {
+    for (var i = 0; i < data.results.length; i++) {
+        myfunc(
+            `<picture>
+                <source media=('min-width: 650px') srcset='${data.results[i].picture.medium}'>
+                <source media=('min-width: 465') srcset='${data.results[i].picture.thumbnail}'>
+                <img src='${data.results[i].picture.large}' alt='Profile Picture' style='width:auto;'>
+            </picture>` + "<br>" + 
+            data.results[i].name.first + "<br>" + 
+            data.results[i].name.last + "<br>" +
+            "Age: " + data.results[i].dob.age
+        );
+    }
 }
 
 function myfunc(somevar) {
-    $('#output').append(somevar + '<br><br>');
+    $("#output").append("<div>" + somevar + "</div>");
 
 }
 
-$('#theHeader').html('<h1>Udemy AJAX</h1>');
-$('#theFooter').html('<h3>Some Stuff In The Footer</h3>');
+$("#theHeader").html("<h1>Udemy AJAX</h1>");
+$("#theFooter").html("<h3>My Lovely Footer Text</h3>");
